43930ff9e2947d4f9ddcc1e8df0b72e059b0867b
Mathieu Acher <mathieu.acher@irisa.fr>
2025-10-02 13:34:52 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/supabase/functions/**']
+    };
+    return config;
+  }
+};
 
 export default nextConfig;
